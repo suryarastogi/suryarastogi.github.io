a03e9d6e97f1e9bbe821b9f720080dd3c76bf768
Surya Rastogi <suryansh@gmail.com>
2023-11-21 06:15:12 +0000

    feat: change icons and revert popover

--- src/components/BlogSearch.tsx
+++ src/components/BlogSearch.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { marked } from 'marked';
-import { Paper, InputBase, IconButton, Divider, Box, Popover } from '@mui/material';
+import { Paper, InputBase, IconButton, Divider, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const BlogSearch: React.FC = () => {
@@ -9,16 +9,19 @@ const BlogSearch: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [selectedPost, setSelectedPost] = useState<string>('');
   const [postContent, setPostContent] = useState<string>('');
-  const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+  const [menuClicked, setMenuClicked] = useState<boolean>(false);
 
   useEffect(() => {
     fetch('/posts/posts.json')
       .then(response => response.json())
       .then(data => {
         setPosts(data);
-        setFilteredPosts(data); // Initialize filteredPosts with all posts
-      });
-  }, []);
+        if (menuClicked) {
+           setFilteredPosts(data);
+        }      
+      }
+    );
+  }, [menuClicked]);
 
   useEffect(() => {
     if (selectedPost) {
@@ -31,29 +34,36 @@ const BlogSearch: React.FC = () => {
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchTerm(value);
-    setAnchorEl(event.currentTarget as unknown as HTMLDivElement); // Correctly cast the target
   
     if (value !== '') {
       const filtered = posts.filter(post => post.includes(value));
       setFilteredPosts(filtered);
     } else {
-      setFilteredPosts(posts);
+      setFilteredPosts([]);
     }
+    setMenuClicked(false);
   };
 
   const handlePostSelect = (post: string) => {
     setSelectedPost(post);
     setSearchTerm(post.replace('.md', ''));
-    setAnchorEl(null); // Close the popover when a post is selected
   };
 
-  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget as unknown as HTMLDivElement); // Correctly cast the target
-    setFilteredPosts(posts);
+  const handleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+    if (!menuClicked) {
+      setFilteredPosts(posts);
+    } else {
+      handleSearchChange({ target: { value: searchTerm }} as React.ChangeEvent<HTMLInputElement>);
+    }
   };
 
   const getHeaderMessage = () => {
-    return searchTerm ? 'Search Matches' : 'All Posts';
+    if (menuClicked) {
+      return 'All Posts';
+    } else {
+      return 'Search Matches';
+    }
   };
 
   return (
@@ -74,24 +84,14 @@ const BlogSearch: React.FC = () => {
           <MenuIcon />
         </IconButton>
       </Paper>
-      <Popover
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        onClose={() => setAnchorEl(null)}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <Box sx={{ maxHeight: 200, overflow: 'auto' }}>
-          <div style={{ padding: '10px' }}>{getHeaderMessage()}</div>
-          {filteredPosts.map((post, index) => (
-            <div key={index} onClick={() => handlePostSelect(post)} style={{ cursor: 'pointer', padding: '10px' }}>
-              {post.replace('.md', '')}
-            </div>
-          ))}
-        </Box>
-      </Popover>
+      <Box sx={{ maxHeight: 200, overflow: 'auto' }}>
+         <div style={{ padding: '10px' }}>{getHeaderMessage()}</div>
+         {filteredPosts.map((post, index) => (
+           <div key={index} onClick={() => handlePostSelect(post)} style={{ cursor: 'pointer', padding: '10px' }}>
+             {post.replace('.md', '')}
+           </div>
+         ))}
+       </Box>
       <div dangerouslySetInnerHTML={{ __html: postContent }} />
     </div>
   );
